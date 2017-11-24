4f473dc27fba125feb7746fd63b169c81077791b
Alexis Janvier <contact@alexisjanvier.net>
2017-11-24 22:37:45 +0100

    Add Helmet for seo

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     plugins: [
         `gatsby-plugin-react-next`,
         `gatsby-plugin-styled-components`,
+        'gatsby-plugin-react-helmet',
         {
             resolve: `gatsby-plugin-typography`,
             options: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
-export default () => <p>Welcome to our new website.</p>;
+export default () => (
+    <div>
+        <Helmet title="CaenCamp">
+            <meta
+                name="description"
+                content="Welcome on the new CaenCamp site"
+            />
+        </Helmet>
+        <p>Welcome to our new website.</p>
+    </div>
+);
