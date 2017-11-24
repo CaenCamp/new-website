6b77e4483d48e0d661c3e1f91aae4654635ed389
Alexis Janvier <contact@alexisjanvier.net>
2017-11-24 19:25:23 +0100

    Upgrade styled-componentt for production

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,4 +3,9 @@ import fairyTheme from 'typography-theme-fairy-gates';
 
 const typography = new Typography(fairyTheme);
 
+// Hot reload typography in development.
+if (process.env.NODE_ENV !== `production`) {
+    typography.injectStyles();
+}
+
 export default typography;
