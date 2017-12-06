cba8f8bc455e4114610d3d85b78e887c6ccf63bf
Alexis Janvier <contact@alexisjanvier.net>
2017-12-06 06:52:13 +0100

    Remove deprecetad component from storybook

--- stories/index.js
+++ stories/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
-import Hello from '../src/components/Hello';
+import { TalkListItem } from '../src/components/talks/listItem';
 
-storiesOf('CaenCamp Components', module).add('Hello component', () => (
-    <Hello who="CaenCamp storybook" />
+storiesOf('CaenCamp Components', module).add('Talks components', () => (
+    <TalkListItem
+        talk={{
+            title: 'Fake talk',
+            edition: 1,
+            slug: 'fake-slug',
+            speakers: [
+                {
+                    firstName: 'John',
+                    lastName: 'Doe',
+                    slug: 'john-doe',
+                },
+            ],
+        }}
+    />
 ));
