204586708f785d3503fc4c13dadcfbb4fafd77af
Gaël Reyrol <gael.reyrol@gmail.com>
2017-12-19 19:56:07 +0100

    Ajout du dernier talk sur la page d'accueil

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,26 +2,88 @@ import { Helmet } from 'react-helmet';
 import React from 'react';
 
 import { Content, LeftColumn } from '../components/Content';
+import { formatTalkWithSpeakers } from '../utils/formatters';
 import SideMenu from '../components/SideMenu';
 
-export default () => (
-    <div>
-        <Helmet title="CaenCamp">
-            <meta
-                name="description"
-                content="Welcome on the new CaenCamp site"
-            />
-        </Helmet>
-        <Content id="homeContent">
-            <LeftColumn>
-                <h1 className="welcome">Welcome to our new website.</h1>
-                <p>
-                    <a className="linkToTalks" href="/talks">
-                        Tous les talks
-                    </a>
-                </p>
-            </LeftColumn>
-            <SideMenu />
-        </Content>
-    </div>
-);
+export default ({ data }) => {
+    const talks = data.talks.edges.map(talk =>
+        formatTalkWithSpeakers(talk.node, data.speakers.edges),
+    );
+
+    let lastTalk = null;
+    if (talks.length) {
+        lastTalk = talks[0];
+    }
+
+    return (
+        <div>
+            <Helmet title="CaenCamp">
+                <meta
+                    name="description"
+                    content="Welcome on the new CaenCamp site"
+                />
+            </Helmet>
+            <Content id="homeContent">
+                <LeftColumn>
+                    <h1 className="welcome">Welcome to our new website.</h1>
+                    <h2 className="lasttalk">Dernier talk</h2>
+                    <h4>
+                        Edition {lastTalk.edition}:{' '}
+                        <a href={`/talks/${lastTalk.slug}`}>{lastTalk.title}</a>
+                    </h4>
+                    <p>
+                        {lastTalk.speakers.length > 0 ? 'par ' : ''}
+                        {lastTalk.speakers.length > 0 &&
+                            lastTalk.speakers.map(
+                                speaker =>
+                                    `${speaker.firstName} ${speaker.lastName},
+                                    `,
+                            )}
+                    </p>
+                    <p>
+                        <a className="linkToTalks" href="/talks">
+                            Tous les talks
+                        </a>
+                    </p>
+                </LeftColumn>
+                <SideMenu />
+            </Content>
+        </div>
+    );
+};
+
+export const query = graphql`
+    query LastTalkQuery {
+        talks: allMarkdownRemark(
+            limit: 1
+            sort: { order: DESC, fields: [frontmatter___edition] }
+            filter: { fileAbsolutePath: { glob: "**/talks/**" } }
+        ) {
+            edges {
+                node {
+                    frontmatter {
+                        edition
+                        title
+                        slug
+                        description
+                        speakers
+                    }
+                }
+            }
+        }
+        speakers: allMarkdownRemark(
+            filter: { fileAbsolutePath: { glob: "**/speakers/**" } }
+        ) {
+            edges {
+                node {
+                    id
+                    frontmatter {
+                        firstName
+                        lastName
+                        slug
+                    }
+                }
+            }
+        }
+    }
+`;
