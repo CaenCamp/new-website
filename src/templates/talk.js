71dc9d06a47f9a6ee6bb31202bc017317f906dfc
Alexis Janvier <contact@alexisjanvier.net>
2018-03-17 16:56:37 +0100

    Fix talk page alignement

--- src/templates/talk.js
+++ src/templates/talk.js
@@ -30,7 +30,7 @@ const DateAndSpeakers = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: center;
+    justify-content: flex-start;
     margin-right: 2rem;
     @media (max-width: ${props => props.theme.mobileSize}) {
         display: none;
