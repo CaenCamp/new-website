fec01dc0bba9f7a0174cf442814678a917cc32df
Alexis Janvier <contact@alexisjanvier.net>
2019-10-27 12:48:09 +0100

    CaenCamp Devops on website - WIP

---
+++ src/pages/caen-camp-devops.js
@@ -0,0 +1,77 @@
+import { graphql } from 'gatsby';
+import { Helmet } from 'react-helmet';
+import React from 'react';
+import styled from 'styled-components';
+
+import Layout from '../components/layout';
+import { Content, SingleColumn } from '../components/Content';
+import CCC from '../components/CodingCaenCamp';
+import { formatGraphContent } from '../utils/formatters';
+import { CampListItem } from '../components/cccs/list-item';
+
+export const Thanks = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    font-weight: bold;
+`;
+
+export default ({ data }) => {
+    const cccs = data.cccs.edges.map(camp => formatGraphContent(camp.node));
+    return (
+        <Layout>
+            <div>
+                <Helmet title="CaenCamp: les Coding Caen Camp (CCC)">
+                    <meta
+                        name="description"
+                        content="Affutez vos skills aux Coding Caen Camp"
+                    />
+                </Helmet>
+                <Content id="dojoContent">
+                    <SingleColumn>
+                        <CCC />
+                        {cccs &&
+                            cccs.map(camp => (
+                                <CampListItem key={camp.id} camp={camp} />
+                            ))}
+                        <Thanks>
+                            <p>
+                                Un grand merci à Emmanuelle et Sylvain de{' '}
+                                <a href="https://www.hey-coworking.com/">
+                                    HEY! coworking
+                                </a>{' '}
+                                pour avoir accepter de nous accueillir une fois
+                                par mois.
+                            </p>
+                        </Thanks>
+                    </SingleColumn>
+                </Content>
+            </div>
+        </Layout>
+    );
+};
+
+export const query = graphql`
+    query CcdsQuery {
+        cccs: allMarkdownRemark(
+            sort: { order: DESC, fields: [frontmatter___date] }
+            filter: { fileAbsolutePath: { glob: "**/ccc/**" } }
+        ) {
+            edges {
+                node {
+                    id
+                    frontmatter {
+                        title
+                        slug
+                        date
+                        description
+                        edition
+                        image
+                    }
+                }
+            }
+        }
+    }
+`;
