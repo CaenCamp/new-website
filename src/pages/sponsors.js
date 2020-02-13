e55a688c59c90717e4aab311580783ea2def0da7
Alexis Janvier <contact@alexisjanvier.net>
2020-02-13 08:24:51 +0100

    Add Sponsor page

---
+++ src/pages/sponsors.js
@@ -0,0 +1,62 @@
+import { Helmet } from 'react-helmet';
+import React from 'react';
+import { graphql } from 'gatsby';
+import styled from 'styled-components';
+
+import { Content, SingleColumn } from '../components/Content';
+import Layout from '../components/layout';
+import { SponsorListItem } from '../components/sponsors/listItem';
+
+export const SponsorContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    align-items: top;
+    justify-content: left;
+`;
+export default ({ data }) => {
+    const sponsors = data.site.siteMetadata.sponsors;
+    return (
+        <Layout>
+            <div>
+                <Helmet title="CaenCamp: les sponsors">
+                    <meta
+                        name="description"
+                        content="Ils nous soutiennent. On leur dit un grand merci !"
+                    />
+                </Helmet>
+                <Content id="welcomeUsContent">
+                    <SingleColumn>
+                        <h1>Les sponsors des CaenCamp.s</h1>
+                        <p>
+                            Ils nous soutiennent ! Nous les remercions
+                            chaleureusement !
+                        </p>
+                        <SponsorContainer>
+                            {sponsors.length > 0 &&
+                                sponsors.map(sponsor => (
+                                    <SponsorListItem
+                                        key={sponsor.name}
+                                        sponsor={sponsor}
+                                    />
+                                ))}
+                        </SponsorContainer>
+                    </SingleColumn>
+                </Content>
+            </div>
+        </Layout>
+    );
+};
+export const query = graphql`
+    query {
+        site {
+            siteMetadata {
+                sponsors {
+                    name
+                    website
+                    logo
+                }
+            }
+        }
+    }
+`;
