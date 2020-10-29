2a635b55cba1cadba388d49ccfd3ef7c8bf34c9b
Clément Le Biez <clement.lebiez@yousign.com>
2020-10-29 08:51:11 +0100

    Remove unused file for e2e testing

--- cypress/integration/flaky.js
+++
@@ -1,7 +0,0 @@
-describe('I am very flaky', () => {
-    it('I can fail or not, it depends.', () => {
-        const int = Math.floor(Math.random() * 10);
-
-        expect(int % 2 === 0).to.equal(true);
-    });
-});
